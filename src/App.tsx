95b045d0752e53773055a0dd33c07bc1e3d879c4
Mochamad Farras Fauzan <farras.fauzan@siklus.com>
2021-12-31 17:50:56 +0700

    Implement AddTodo component to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import TodoItem from "./components/molecules/todoItem";
+import AddTodo from "./components/molecules/addTodo";
 import Navbar from "./components/molecules/navbar";
+import TodoItem from "./components/molecules/todoItem";
 
 // Todo App
 // 1. Implement tailwind css
@@ -15,6 +15,7 @@ function App() {
       <div className="flex flex-col justify-start bg-gradient-to-r from-cyan-700 to-sky-700 w-72 h-80 rounded-lg p-3">
         <Navbar />
         <TodoItem title="Mandi" />
+        <AddTodo />
       </div>
     </div>
   );
