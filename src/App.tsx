d4bf804473a72195a8e15adfaa5a84f4135f940a
Mochamad Farras Fauzan <farras.fauzan@siklus.com>
2021-12-31 14:39:02 +0700

    Implement navbar to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import TodoItem from "./components/molecules/todoItem";
+import Navbar from "./components/molecules/navbar";
 
 // Todo App
 // 1. Implement tailwind css
@@ -11,7 +12,8 @@ import TodoItem from "./components/molecules/todoItem";
 function App() {
   return (
     <div className="flex justify-center items-center bg-gradient-to-r from-gray-800 to-gray-600 h-screen">
-      <div className="flex justify-center bg-gradient-to-r from-cyan-700 to-sky-700 w-72 h-80 rounded-lg p-3">
+      <div className="flex flex-col justify-start bg-gradient-to-r from-cyan-700 to-sky-700 w-72 h-80 rounded-lg p-3">
+        <Navbar />
         <TodoItem title="Mandi" />
       </div>
     </div>
