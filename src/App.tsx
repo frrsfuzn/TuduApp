25b97e79c519dbf3b9cbffa45a662215e43e8887
Mochamad Farras Fauzan <farras.fauzan@siklus.com>
2021-12-31 13:30:44 +0700

    Implement TodoItem to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import CheckBox from "./components/atoms/checkBox";
+import TodoItem from "./components/molecules/todoItem";
 
 // Todo App
 // 1. Implement tailwind css
@@ -12,10 +12,7 @@ function App() {
   return (
     <div className="flex justify-center items-center bg-gradient-to-r from-gray-800 to-gray-600 h-screen">
       <div className="flex justify-center bg-gradient-to-r from-cyan-700 to-sky-700 w-72 h-80 rounded-lg p-3">
-        <div className="flex flex-row px-3 items-center bg-gradient-to-r from-cyan-800 to-sky-800 h-12 rounded-lg w-full shadow-md text-white">
-          <CheckBox />
-          <h3 className="text-xl">Mandi</h3>
-        </div>
+        <TodoItem title="Mandi" />
       </div>
     </div>
   );
