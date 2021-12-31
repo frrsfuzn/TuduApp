0ed75053c5fed61daafd0b0fd6ba48ea2cc190aa
Mochamad Farras Fauzan <farras.fauzan@siklus.com>
2021-12-31 10:27:35 +0700

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+// Todo App
+// 1. Implement tailwind css
+// 2. using hooks (useRef, useReducer, useCallback, useMemo)
+// 3. using input type color
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex justify-center items-center bg-gradient-to-r from-gray-800 to-gray-600 h-screen">
+      <div className="flex justify-center bg-gradient-to-r from-cyan-700 to-sky-700 w-72 h-80 rounded-lg p-3">
+        <div className="flex flex-row justify-center items-center bg-red-600 h-12 rounded-lg w-full">
+          <input type="checkbox" />
+          <h3 className="text-xl">Mandi</h3>
+        </div>
+      </div>
     </div>
   );
 }
