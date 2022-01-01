2ade88484676ff9a2af048d355a72e319b13a45b
Mochamad Farras Fauzan <farras.fauzan@siklus.com>
2022-01-01 17:26:39 +0700

    remove comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,11 @@
 import React from "react";
 import "./App.css";
-import AddTodo from "./components/molecules/addTodo";
-import Navbar from "./components/molecules/navbar";
-import TodoItem from "./components/molecules/todoItem";
-
-// Todo App
-// 1. Implement tailwind css
-// 2. using hooks (useRef, useReducer, useCallback, useMemo)
-// 3. using input type color
+import Tudu from "./components/organisms/tudu";
 
 function App() {
   return (
     <div className="flex justify-center items-center bg-gradient-to-r from-gray-800 to-gray-600 h-screen">
-      <div className="flex flex-col justify-start bg-gradient-to-r from-cyan-700 to-sky-700 w-72 h-80 rounded-lg p-3">
-        <Navbar />
-        <TodoItem title="Mandi" />
-        <AddTodo />
-      </div>
+      <Tudu />
     </div>
   );
 }
